e262123cb466991f54633fef268bc847d8a16adc
Dinh Chuong <chuongdinh2202@gmail.com>
2023-03-13 12:14:55 +0700

    update

--- src/main/java/com/athttt/controllers/ShopController.java
+++ src/main/java/com/athttt/controllers/ShopController.java
@@ -1,11 +1,12 @@
 package com.athttt.controllers;
 
+import java.util.stream.Collectors;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.athttt.entity.Product;
 import com.athttt.service.ProductService;
@@ -13,23 +14,28 @@ import com.athttt.service.ProductService;
 @Controller
 public class ShopController {
 	@Autowired
-	ProductService productService ;
-	
+	ProductService productService;
+
 	@RequestMapping("/shop")
-	public String index(Model model) {
-		List<Product> listProduct =  productService.getAllProduct();
+	public String index(Model model ) {
+		
+		List<Product> listProduct = productService.getAllProduct();
+
 		model.addAttribute("products", listProduct);
+
 		return "shop";
 	}
+
 	@RequestMapping("/shop/{id}")
 	public String shopDetail() {
 		return "shop-details";
 	}
+
 	@RequestMapping("/my-cart")
 	public String myCart() {
 		return "shopping-cart";
 	}
-	
+
 	@RequestMapping("/checkout")
 	public String checkout() {
 		return "checkout";
